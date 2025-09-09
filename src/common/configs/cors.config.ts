f6efdfac8955686ad348c72adc0e91028d8b008d
Victor - Trabalho <victor.samuelsantoss@gmail.com>
2025-09-08 21:45:47 -0300

    chore: update cors.config

--- src/common/configs/cors.config.ts
+++ src/common/configs/cors.config.ts
@@ -1,24 +1,8 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsConfig: CorsOptions = {
-  origin: ['https://central-da-fe-frontend.vercel.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-    'Accept',
-    'Accept-Language',
-    'Cache-Control',
-    'Pragma',
-    'Priority',
-    'Sec-Fetch-Dest',
-    'Sec-Fetch-Mode',
-    'Sec-Fetch-Site',
-    'User-Agent',
-    'Origin',
-    'Referer',
-  ],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
-  optionsSuccessStatus: 200,
-  preflightContinue: false,
 };
